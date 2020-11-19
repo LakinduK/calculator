2a166f6707d6a21fcaae6e7dc8f69bc498b92997
LakinduK <lakindu1997@gmail.com>
2020-11-19 22:03:30 +0530

    added +.-,/,* func

--- widget.cpp
+++ widget.cpp
@@ -1,6 +1,10 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include <QDebug>
+
+//globle variables
+double firstNum;
+
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
@@ -17,6 +21,16 @@ Widget::Widget(QWidget *parent)
     connect(ui->btn7,SIGNAL(released()),this,SLOT(digit_pressed()));
     connect(ui->btn8,SIGNAL(released()),this,SLOT(digit_pressed()));
     connect(ui->btn9,SIGNAL(released()),this,SLOT(digit_pressed()));
+
+    connect(ui->btn_add,SIGNAL(released()),this,SLOT(binary_operation_pressed()));
+    connect(ui->btn_sub,SIGNAL(released()),this,SLOT(binary_operation_pressed()));
+    connect(ui->btn_div,SIGNAL(released()),this,SLOT(binary_operation_pressed()));
+    connect(ui->btn_mul,SIGNAL(released()),this,SLOT(binary_operation_pressed()));
+
+    ui->btn_add->setCheckable(true);
+    ui->btn_sub->setCheckable(true);
+    ui->btn_mul->setCheckable(true);
+    ui->btn_div->setCheckable(true);
 }
 
 Widget::~Widget()
@@ -31,7 +45,16 @@ void Widget::digit_pressed()
     double labelNumber;
     QString newLabel;
 
-    labelNumber = (ui->label->text() + button->text()).toDouble();
+    if(ui->btn_add->isChecked() ||ui->btn_sub->isChecked() ||ui->btn_mul->isChecked() ||
+            ui->btn_div->isChecked())
+    {
+         labelNumber = button->text().toDouble();
+    }
+    else
+    {
+        labelNumber = (ui->label->text() + button->text()).toDouble();
+    }
+
 
     newLabel = QString::number(labelNumber,'g',15);
     ui->label->setText(newLabel);
@@ -50,13 +73,47 @@ void Widget::on_btn_clear_released()
 
 void Widget::on_btn_equal_released()
 {
+    double labelNumber, secondNum;
+    QString newLabel;
 
+    secondNum = ui->label->text().toDouble();
+
+    if(ui->btn_add->isChecked())
+    {
+        labelNumber = firstNum + secondNum;
+        newLabel = QString::number(labelNumber,'g',15);
+        ui->label->setText(newLabel);
+        ui->btn_add->setChecked(false);
+    }
+    else if(ui->btn_sub->isChecked())
+    {
+        labelNumber = firstNum - secondNum;
+        newLabel = QString::number(labelNumber,'g',15);
+        ui->label->setText(newLabel);
+        ui->btn_sub->setChecked(false);
+    }
+    else if(ui->btn_mul->isChecked())
+    {
+        labelNumber = firstNum * secondNum;
+        newLabel = QString::number(labelNumber,'g',15);
+        ui->label->setText(newLabel);
+        ui->btn_mul->setChecked(false);
+    }
+    else if(ui->btn_div->isChecked())
+    {
+        labelNumber = firstNum / secondNum;
+        newLabel = QString::number(labelNumber,'g',15);
+        ui->label->setText(newLabel);
+        ui->btn_div->setChecked(false);
+    }
 }
 
 
 void Widget::binary_operation_pressed()
 {
     QPushButton * button = (QPushButton*)sender();
+    firstNum = ui->label->text().toDouble();
+    button->setChecked(true);
 }
 
 
